bbeb2ec0bcc39e5bb6bd3c76923d4daa01464ac7
nasir <nasir.niazi@tekschool.us>
2022-06-22 17:40:56 -0700

    test

---
+++ src/gitHubcLass/Test2.java
@@ -0,0 +1,13 @@
+package gitHubcLass;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+System.out.println(" the first day exercise");
+int a=20;
+int x=30;
+System.out.println(a+x
+		);
+	}
+
+}
---
+++ src/gitHubcLass/dayOne.java
@@ -0,0 +1,10 @@
+package gitHubcLass;
+
+public class dayOne {
+public static void main(String[] args) {
+	int a = 20;
+	int x = 30;
+	
+	System.out.println(a+x+"The first day of GitHubClass");
+}
+}
